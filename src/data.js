06554646f514923f5243210a4869fc5f0f850a49
MarlenaFlueh <mf@esyoil.com>
2018-09-02 13:50:45 +0200

    made api requiest

--- src/data.js
+++ src/data.js
@@ -1,1 +1,11 @@
 export const tempData = [22, 24, 15, 13];
+
+const url = "/temperatures";
+
+fetch(url)
+  .then(data => data.json())
+  .then(res => {
+    const tempArray = [];
+    res.temperatures.map(res => tempArray.push(res.temp));
+    console.log(tempArray);
+  });
