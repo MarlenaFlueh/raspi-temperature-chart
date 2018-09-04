ea7ec7cc2f59b48d465aec12c3afb11b57e35448
MarlenaFlueh <mf@esyoil.com>
2018-09-04 14:35:18 +0200

    delete restful api code and npm packages

--- src/api/model/temperature.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const temperatureSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  temp: { type: Number, required: true }
-});
-
-module.exports = mongoose.model("Temperature", temperatureSchema);
--- src/server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const app = require("./serverApp");
-
-const port = process.env.PORT || 5000;
-
-const server = http.createServer(app);
-
-server.listen(port);
