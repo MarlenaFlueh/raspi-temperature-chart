6d5f988e24dba2eeea222dcdd0fa73d458d22e8a
MarlenaFlueh <mf@esyoil.com>
2018-09-08 00:49:29 +0200

    create info text

--- src/App.js
+++ src/App.js
@@ -2,29 +2,40 @@ import React, { Component } from "react";
 import { Bar } from "react-chartjs-2";
 
 import tempData from "./data";
-import Raspberry from "./utils/img/Raspberry_Pi_B+.svg";
+import Restful from "./utils/img/restapi.png";
 
 const divStyle = {
   position: "relative",
   height: "100vh",
-  width: "60vw",
+  width: "50vw",
   margin: "auto"
 };
 
 const headingStyle = {
   "font-weight": "lighter",
-  "text-align": "center"
+  "text-align": "center",
+  "padding-top": "2rem"
 };
 
 const textStyle = {
   "font-size": "1.1rem",
-  padding: "1rem",
   "line-height": "2"
 };
 
-const raspStyle = {
+const RestapiStyle = {
   height: "40%",
   width: "40%"
+};
+
+const olStyle = {
+  "font-size": "1.1rem",
+  "line-height": "1.5"
+};
+
+const strongStyle = {
+  "font-size": "1.1rem",
+  "font-weight": "bold",
+  "padding-top": "1rem"
 };
 
 class App extends Component {
@@ -62,15 +73,30 @@ class App extends Component {
       <div style={divStyle}>
         <h1 style={headingStyle}>Measure temperature with the Raspberry Pi</h1>
         <p style={textStyle}>
-          Have you ever wondered what to do as a next project with your
-          Raspberry Pi? What about measuring the temperature and represent it in
-          a chart - everything you need is a Raspberry Pi and a temperature
-          sensor. You receive the temperature data with the Raspi, then you send
-          the data to a Restful API, connected with a database. To display the
-          chart, you just get the chart data from the Restful API.
+          Have you ever wondered what to do as a next Raspberry Pi project? What
+          about measuring the temperature and represent it in a chart -
+          everything you need is a Raspberry Pi and a temperature sensor. You
+          receive the temperature data with the Raspi, then you send the data to
+          a Restful API, connected with a database. To display the chart, you
+          just get the chart data from the Restful API.
         </p>
-        <img src={Raspberry} style={raspStyle} alt="Raspberry" />
         <Bar data={this.state.chartData} />
+        <p style={strongStyle}>Possible structure:</p>
+        <ol style={olStyle}>
+          <li>create Restful API with MongoDB Atlas connection</li>
+          <li>receive data with Raspberry Pi from the temperature sensor</li>
+          <li>display the temperature as a chart with a React.js app</li>
+        </ol>
+        <p style={strongStyle}>1. Restful API</p>
+        <p style={textStyle}>
+          A Restful API is a programming interface and uses HTTP requests to
+          handle data (PUT, GET and DELETE). The API simplifies the
+          communication of computer programs. APIs also allows to interact with
+          cloud services like the MongoDB Atlas database. After deploying it can
+          look like that:
+        </p>
+        <img src={Restful} style={RestapiStyle} alt="Restful API" />
+        <p style={strongStyle}>2. Raspberry Pi and temperature sensor</p>
       </div>
     );
   }
